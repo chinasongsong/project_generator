fc6d7ba2d6e2025d39cd1ef1ba057f83e8e8c964
chinasongsong <1277342651@qq.com>
2025-08-27 23:09:17 +0800

    feat: init common code

--- src/main/java/com/ai/project/project_generator/ProjectGeneratorApplication.java
+++ src/main/java/com/ai/project/project_generator/ProjectGeneratorApplication.java
@@ -2,8 +2,11 @@ package com.ai.project.project_generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
+
 public class ProjectGeneratorApplication {
 
     public static void main(String[] args) {
