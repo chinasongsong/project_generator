1d45090570d27eaf33ae31e4cf9195cb9a134ac6
chinasongsong <1277342651@qq.com>
2025-11-13 20:53:31 +0800

    fix: 适配redis 6.x版本

--- src/main/java/com/ai/project/project_generator/config/RedisChatMemoryStoreConfig.java
+++ src/main/java/com/ai/project/project_generator/config/RedisChatMemoryStoreConfig.java
@@ -6,7 +6,6 @@ package com.ai.project.project_generator.config;
 
 import dev.langchain4j.community.store.memory.chat.redis.RedisChatMemoryStore;
 import lombok.Data;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,13 +29,16 @@ public class RedisChatMemoryStoreConfig {
 
     private long ttl;
 
+    private String username;
+
     @Bean
     public RedisChatMemoryStore redisChatMemoryStore() {
         return RedisChatMemoryStore.builder()
-            .host(host)
-            .port(port)
-            .password(password)
-            .ttl(ttl)
-            .build();
+                .host(host)
+                .port(port)
+                .password(password)
+                .ttl(ttl)
+                .user(username)
+                .build();
     }
 }
